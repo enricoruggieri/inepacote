7a63957a75ca7bc78e0af0a6ee494664a507aab6
enricoruggieri <eruggieri@gmail.com>
2025-03-05 00:51:31 -0300

    renaming

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="synth_sampler",
+    name="inepirata",
     version="0.1.0",
     packages=find_packages(),
     install_requires=["pandas"],
     include_package_data=True,
-    package_data={"synth_sampler": ["synth_model.pkl"]},
+    package_data={"inepirata": ["synth_model.pkl"]},
     python_requires=">=3.6",
 )
