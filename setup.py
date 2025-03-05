d0f90071a63cb9aff6fc18fee9e6ef2cba7ce4e5
enricoruggieri <eruggieri@gmail.com>
2025-03-05 08:48:40 -0300

    inepacote

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="inepirata",
+    name="inepacote",
     version="0.1.0",
     packages=find_packages(),
     install_requires=["pandas"],
     include_package_data=True,
-    package_data={"inepirata": ["synth_model.pkl"]},
+    package_data={"inepacote": ["synth_model.pkl"]},
     python_requires=">=3.6",
 )
